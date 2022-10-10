1ac6021d3cf4b5b9fbff902254229af1f35b72f7
Martin William <martinsoftwaredev@outlook.com>
2022-10-10 14:27:22 +0100

    connect to firebase, send data on submit

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { setDoc, doc } from 'firebase/firestore';
+// login details for firebase
+import { firebaseConfig } from './config';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// variables for connection to form
+const username = 'username1';
+const email = 'username@gmail.com';
+const password = 'password123';
+
+const addUser = async () => {
+  const setDocDetails = setDoc(doc(db, 'users', `${username}`), {
+    username: username,
+    email: email,
+    password: password,
+  });
+  await setDocDetails;
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="submit" onPress={addUser}></Button>
       <StatusBar style="auto" />
     </View>
   );
