d91da6e964766756d8e046d33ba2752a1fe807b3
BrokenRecord85 <pawel_kunicki@hotmail.com>
2022-10-14 22:33:20 +0100

    Distance calc test

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -5,6 +5,7 @@ import MapView from 'react-native-maps';
 import { Marker } from 'react-native-maps';
 import {getUsers, } from '../../queryutils'
 import { useNavigation } from '@react-navigation/native';
+import { getDistance } from 'geolib';
 
 import * as Location from 'expo-location';
 
@@ -25,6 +26,8 @@ export default function MyLocation() {
   const[liveLoading, setLiveLoading] = useState(false)
 
   const params = navigation.getState().routes[0].params;
+
+  console.log(params)
 
   useEffect(() => {
     setIsLoading(true)
@@ -56,6 +59,10 @@ export default function MyLocation() {
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       });
+      setMyLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      })
       
     };
     useEffect(() => {
@@ -68,12 +75,19 @@ export default function MyLocation() {
           
       
  
-      
+  //  dancers.map((dancer) => {
+  //   const distance = getDistance(dancer.location, myLocation)
+  //   //console.log(`${Math.round((distance/1000)*0.62137)}miles from ${dancer.firstname}`)
+  //   const distanceMiles = Math.round((distance/1000)*0.62137)
+  //   if (distanceMiles <= 5) {
+  //     //console.log(dancer.firstname)
+  //   }
+  //  })
     
     
         
      
-  console.log(dancers)
+  //console.log(dancers)
   
   
   
@@ -97,9 +111,23 @@ export default function MyLocation() {
                 }}
                 title="Marker" pinColor='blue'/> */}
               
-          {dancers?.map((dancer, index) => {
+          {/* {dancers?.map((dancer, index) => {
             return <Marker key={index} coordinate={dancer.location} title={dancer.firstname}/>   
-          })}
+          })} */}
+
+    {/* {dancers.map((dancer, index) => {
+    const distance = getDistance(dancer.location, myLocation)
+    //console.log(`${Math.round((distance/1000)*0.62137)}miles from ${dancer.firstname}`)
+    const distanceMiles = Math.round((distance/1000)*0.62137)
+    {distanceMiles <= 5? <Marker key={index} coordinate={dancer.location} title={dancer.firstname}/> : <></>
+      
+    }
+    })} */}
+
+    {dancers.filter((dancer) => (Math.round((getDistance(dancer.location, myLocation)/1000)*0.62137)) <=4)
+    .map((filteredDancer, index) => {
+      return <Marker key={index} coordinate={filteredDancer.location} title={filteredDancer.firstname}/>
+    })}
             
         
          <Marker
@@ -122,6 +150,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 20,
   },
   map: {
     width: 400,
