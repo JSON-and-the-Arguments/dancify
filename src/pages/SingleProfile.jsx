5effd409f6fadd7ea657edbfbf3603737a895b51
BrokenRecord85 <pawel_kunicki@hotmail.com>
2022-10-20 10:21:58 +0100

    Few fixes

--- src/pages/SingleProfile.jsx
+++ src/pages/SingleProfile.jsx
@@ -11,6 +11,7 @@ import Navbar from "../components/Navbar";
 import { useNavigation } from "@react-navigation/native";
 import { getUser } from "../../queryutils";
 import AddMessage from "../components/AddMessage";
+import ScreenTemplate from "../components/ScreenTemplate";
 
 const SingleProfile = ({ route, navigation }) => {
   const { user } = route.params;
@@ -23,27 +24,29 @@ const SingleProfile = ({ route, navigation }) => {
   }, []);
 
   return (
-    <View>
+    <ScreenTemplate>
+    <View className='items-center'>
       <Navbar />
-      <View className="bg-red-100 items-center border-4 border-red-500/10 m-5  h-120 rounded  ">
-        <View className="p-8">
+      <View className="bg-transparent w-80 items-center  m-5  h-120 rounded  ">
+        <View className="p-8 w-full">
           <Image
-            className=" w-60 h-60 items- border-solid border-4 border-red-500/10 rounded-lg "
+            className=" w-full h-60  border-solid border-4  rounded-lg "
             source={{
               uri: `https://storage.googleapis.com/dancify-728c9.appspot.com/userPictures/${profile.uid}/profilePicture.jpeg`,
             }}
           />
         </View>
-        <View className="p-10 pt-5 list-decimal">
-          <Text className="font-semibold">Name: {profile.firstname}</Text>
-          <Text className="font-semibold">Last name: {profile.lastname}</Text>
-          <Text className="font-semibold">style: {profile.dancestyles}</Text>
-          <Text className="font-semibold">role: {profile.role}</Text>
-          <Text className="font-semibold">about: {profile.about}</Text>
+        <View className="space-y-3">
+          <Text className="font-semibold text-white text-4xl capitalize">{profile.firstname} {profile.lastname} </Text>
+          <Text className="font-semibold text-white text-xl capitalize">{profile.dancestyles}</Text>
+          <Text className="font-semibold text-white text-xl capitalize">{profile.role}</Text>
+          <Text className="font-semibold text-white text-xl">About me:</Text>
+          <Text className="font-semibold text-white text-xl">{profile.about}</Text>
         </View>
         <AddMessage user={user} />
       </View>
     </View>
+    </ScreenTemplate>
   );
 };
 
